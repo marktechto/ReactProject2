ff690c2f983dec7bf7abc4ffee99a4fccc301483
MarkoTeshomeTECH <zeanbesa7066@gmail.com>
2024-09-06 21:30:05 +0300

    JS Array Method RememberingAssynchraneous and promise

--- script.js
+++ script.js
@@ -190,3 +190,6 @@ const newBook = {
 };
 const UpdatedBooks = [...books, newBook];
 UpdatedBooks;
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
